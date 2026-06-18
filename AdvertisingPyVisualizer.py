62f59e88b06835ab00dc8ac1692934bb9d5b5f12
yassin595 <yassinmostafamahmoud@gmail.com>
2026-06-18 19:22:29 +0300

    This change adds the visualization for the dataset

--- AdvertisingPyVisualizer.py
+++ AdvertisingPyVisualizer.py
@@ -2,3 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+x = pd.read_csv('Advertising.csv')
+plt.scatter(x['TV'], x['Sales'])
+plt.title('TV vs Sales')
+plt.xlabel('TV')
+plt.ylabel('Sales')
+plt.show()
+plt.scatter(x['Radio'], x['Sales'])
+plt.title('Radio vs Sales')
+plt.xlabel('Radio')
+plt.ylabel('Sales')
+plt.show()
+plt.scatter(x['Newspaper'], x['Sales'])
+plt.title('Newspaper vs Sales')
+plt.xlabel('Newspaper')
+plt.ylabel('Sales')
+plt.show()
